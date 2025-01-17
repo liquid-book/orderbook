571ae74c826062463022a3a823cdc79d5d63c8b1
renakaagusta <renakaagusta28@gmail.com>
2025-01-17 22:50:08 +0700

    feat: update readme and ponder config

--- indexer/ponder.config.ts
+++ indexer/ponder.config.ts
@@ -7,6 +7,9 @@ import { TickManagerABI } from "./abis/TickManagerABI";
 import { BitmapManagerABI } from "./abis/BitmapManagerABI";
 import { MatcherABI } from "./abis/MatcherABI";
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default createConfig({
   networks: {
     arbitrumSepolia: {
@@ -15,35 +18,53 @@ export default createConfig({
     },
   },
   contracts: {
+    PoolManager: {
+      network: "arbitrumSepolia",
+      abi: PoolManagerABI,
+      address: process.env.POOL_MANAGER_ADDRESS as `0x${string}`,
+      startBlock: Number(process.env.POOL_MANAGER_BLOCK),
+    },
+    PoolOrderbook: {
+      network: "arbitrumSepolia",
+      abi: PoolOrderbookABI,
+      address: process.env.POOL_ORDERBOOK_ADDRESS as `0x${string}`,
+      startBlock: Number(process.env.POOL_ORDERBOOK_BLOCK),
+    },
+    BalanceManager: {
+      network: "arbitrumSepolia",
+      abi: BalanceManagerABI,
+      address: process.env.BALANCE_MANAGER_ADDRESS as `0x${string}`,
+      startBlock: Number(process.env.BALANCE_MANAGER_BLOCK),
+    },
     Engine: {
       network: "arbitrumSepolia",
       abi: EngineABI,
-      address: "0x4307c7da3fa5a45a8e280bad6bbe01b57a3b4e4e",
-      startBlock: 114371569,
+      address: process.env.ENGINE_ADDRESS as `0x${string}`,
+      startBlock: Number(process.env.ENGINE_BLOCK),
     },
     TickManager: {
       network: "arbitrumSepolia",
       abi: TickManagerABI,
-      address: "0xe61baf90143c169632d198809a44a20357d2c7d2",
-      startBlock: 114371602,
+      address: process.env.TICK_ADDRESS as `0x${string}`,
+      startBlock: Number(process.env.TICK_BLOCK),
     },
     OrderManager: {
       network: "arbitrumSepolia",
       abi: OrderManagerABI,
-      address: "0x0e10b18ac7eec8fb3d85f3e532dfae381c7bca95",
-      startBlock: 114371533,
+      address: process.env.ORDER_ADDRESS as `0x${string}`,
+      startBlock: Number(process.env.ORDER_BLOCK),
     },
     BitmapManager: {
       network: "arbitrumSepolia",
       abi: BitmapManagerABI,
-      address: "0xd0061c14d056d4ee06d8b048f3a7bc0304396c31",
-      startBlock: 114371585,
+      address: process.env.BITMAP_ADDRESS as `0x${string}`,
+      startBlock: Number(process.env.BITMAP_BLOCK),
     },
     Matcher: {
       network: "arbitrumSepolia",
       abi: MatcherABI,
-      address: "0x993c639ddadc5c427281c1adbc2dfef122be98cb",
-      startBlock: 114371555,
-    },
+      address: process.env.MATCHER_ADDRESS as `0x${string}`,
+      startBlock: Number(process.env.MATCHER_BLOCK),
+    }
   },
 });
